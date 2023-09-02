de532c7777c30d6d13af03d4ed51ce5aabd30aae
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2023-09-02 04:12:51 +0300

    added testing environment for backend

--- bloglist-frontend/cypress/e2e/spec.cy.js
+++ bloglist-frontend/cypress/e2e/spec.cy.js
@@ -1,3 +1,5 @@
+import LoginForm from '../../src/components/LoginForm'
+
 describe('Blog app', function() {
   beforeEach(function() {
     //cy.request('POST', 'http://localhost:3003/api/testing/reset')
@@ -5,9 +7,30 @@ describe('Blog app', function() {
   })
 
   it('Login form is shown', function() {
+    cy.get('.login-form-container').should('exist')   // LoginFrom is present
     cy.contains('log in to application')
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.visit('http://localhost:5173')
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('passwd')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.visit('http://localhost:5173')
+      cy.get('#username').type('non-existent user')
+      cy.get('#password').type('not a real password')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong username or password')
+    })
   })
 })
--- bloglist-frontend/src/components/LoginForm.jsx
+++ bloglist-frontend/src/components/LoginForm.jsx
@@ -29,12 +29,14 @@ const LoginForm = ({ setUser, setErrorMessage }) => {
   }
 
   return (
-    <div>
+    // Has a className for Cypress testing
+    <div className='login-form-container'>
       <h2>log in to application</h2>
       <form onSubmit={handleLogin}>
         <div>
           username
           <input
+            id='username'
             type="text"
             value={username}
             name="Username"
@@ -44,13 +46,18 @@ const LoginForm = ({ setUser, setErrorMessage }) => {
         <div>
           password
           <input
+            id='password'
             type="password"
             value={password}
             name="Password"
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
-        <button type="submit">login</button>
+        <button
+          id='login-button'
+          type="submit">
+            login
+        </button>
       </form>
     </div>
   )
